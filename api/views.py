fc1a617ad4bbc5bb2b609f0e9f2b90abaf82959c
AbhishekMore-1 <abhishekbabajim@gmail.com>
2021-12-20 16:23:58 +0530

    Added Search and Sorting Filters

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from .models import youtubeVideo
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from .serializers import youtubeSerializer
 from rest_framework.pagination import PageNumberPagination
 
@@ -9,11 +9,19 @@ class CustomPagination(PageNumberPagination):
     page_size_query_param = 'pageSize'
     max_page_size = 1000
 
+
+# Configuration for API and Browsable API view
 class youtubeVideos(viewsets.ModelViewSet):
     """
     API endpoint that allows Youtube Video list to be viewed
     """
     # response sorted in descending order of published datetime
     queryset = youtubeVideo.objects.all().order_by('-publishDatetime')
+
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter)
+    # Adding the search and filter fields
+    search_fields = ['videoId', 'videoTitle', 'description', ]
+    filter_fields = ['publishDatetime']
+    ordering_fields = ['publishDatetime']
     serializer_class = youtubeSerializer
     pagination_class = CustomPagination
