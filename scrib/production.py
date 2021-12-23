1e1fbe5253ea33c4a094318dd81a133fef102d11
AbhishekMore-1 <abhishekbabajim@gmail.com>
2021-12-23 20:27:33 +0530

    API serving HTTP page links solved

--- scrib/production.py
+++ scrib/production.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Quick-start development settings - unsuitable for production
